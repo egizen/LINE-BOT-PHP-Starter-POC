d9824fcccddfd7ec63b62aa886ba5b214095088e
saranyoo.s <saranyoo.s@kbtg.tech>
2017-03-02 19:40:31 +0700

    test

--- BotTest.php
+++ BotTest.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && ((strpos($event['message']['text'], 'Man') !== FALSE) || (strpos($event['message']['text'], 'แมน') !== FALSE))) {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && ((strpos($event['message']['text'], 'Man') !== FALSE) || (strpos($event['message']['text'], 'แมน') !== FALSE) || (strpos($event['message']['text'], 'สวัสดี007') !== FALSE))) {
 			// Get text sent
 			$text = 'พี่แมนมีคนเรียกมาตอบเร็วๆๆๆๆๆๆๆๆๆๆ';
 			
