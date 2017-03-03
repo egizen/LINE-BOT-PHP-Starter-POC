35a6a641949ee842034dcc1da4a755124782d2e1
saranyoo.s <saranyoo.s@kbtg.tech>
2017-03-03 13:08:36 +0700

    test

--- BotLine/callback.php
+++ BotLine/callback.php
@@ -1,6 +1,12 @@
 <?php // callback.php
 echo "OK";
 
+ob_start();
+$raw = file_get_contents('php://input');
+var_dump(json_decode($raw,1));
+$raw = ob_get_clean();
+file_put_contents('/tmp/dump.txt', $raw."\n=====================================\n", FILE_APPEND);
+
 define("LINE_MESSAGING_API_CHANNEL_SECRET", '0741b7e3da24f7b071637fb731ef1777');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'VTECj7jjrSAqG8amUkWnSTxghAsTISeZTAxeTq0cYkKkS1M8LfKl2dX/4lkoarWE9XlqL3fYmdBPiowrlfKEiiD44r/HgLFwt4O4GNU5ZDvAjJB4uesc04SqAIuHcNv0PjvpfPQKjg+VfSbkAuV+CQdB04t89/1O/w1cDnyilFU=');
 
