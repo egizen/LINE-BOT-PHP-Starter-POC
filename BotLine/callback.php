01a91d3d9c826b8435e7763dd08cea668e3356f7
saranyoo.s <saranyoo.s@kbtg.tech>
2017-03-03 19:12:20 +0700

    test

--- BotLine/callback.php
+++ BotLine/callback.php
@@ -43,6 +43,28 @@ foreach ($events as $event) {
 			$reply_token = $event->getReplyToken();
 			$bot->replyText($reply_token, $result_text);
 		}
+		
+		if (strpos($text, '007 อากาศ') !== FALSE) {
+			$text_ex = explode(' ', $text);
+			$ch1 = curl_init();
+			curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch1, CURLOPT_URL, 'http://api.wunderground.com/api/487aafe9ed0df755/forecast/lang:TH/q/Thailand/'.str_replace(' ', '%20', $text_ex[2]).'.json');
+			$result1 = curl_exec($ch1);
+			curl_close($ch1);
+			
+			$obj = json_decode($result1, true);
+			
+			if(isset($obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'])){
+				$result_text = $obj['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric'];
+			}
+			else(empty($result_text)){
+				$result_text = 'ไม่พบข้อมูล';
+			}
+			
+			$reply_token = $event->getReplyToken();
+			$bot->replyText($reply_token, $result_text);
+		}
     }
 	
 	
