dab4370be9a421dbaaefec0a6326043048b2f21a
saranyoo.s <saranyoo.s@kbtg.tech>
2017-03-06 12:19:05 +0700

    test

--- BotLine/callback.php
+++ BotLine/callback.php
@@ -21,14 +21,21 @@ foreach ($events as $event) {
 		
 		$text = $event->getText();
 		
-		if($event->isGroupEvent()) {
+		//get group id
+		if($event->isGroupEvent() {
 			$text22 = $event->getGroupId();
-			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text22);
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
+			$response = $bot->pushMessage('C92ba367859d8098c1b4308ca158150a0', $textMessageBuilder);
+		}
+		
+		//fwd mobile inc from war room to team group.
+		if($event->isGroupEvent() && $event->getGroupId() == 'C92ba367859d8098c1b4308ca158150a0' && strpos($text, 'mobile') !== FALSE) {
+			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
 			$response = $bot->pushMessage('C92ba367859d8098c1b4308ca158150a0', $textMessageBuilder);
 		}
 
 		
- 		if (strpos($text, '007 อยากรู้') !== FALSE) {
+ 		if (strpos($text, '007 อยากรู้') !== FALSE) { //get info from wiki.
 			$text_ex = explode(' ', $text);
 			$ch1 = curl_init();
 			curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
@@ -46,7 +53,7 @@ foreach ($events as $event) {
 			if(empty($result_text)){
 				$result_text = 'ไม่พบข้อมูล';
 			}
-		}else if (strpos($text, '007 อากาศ') !== FALSE) {
+		}else if (strpos($text, '007 อากาศ') !== FALSE) { //get weather from api.
 			$text_ex = explode(' ', $text);
 			$ch2 = curl_init();
 			curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
