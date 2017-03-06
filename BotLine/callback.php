f9f0f8a6488124c156e4412d216126cb30706ed5
saranyoo.s <saranyoo.s@kbtg.tech>
2017-03-06 17:27:23 +0700

    wwww

--- BotLine/callback.php
+++ BotLine/callback.php
@@ -50,14 +50,14 @@ foreach ($events as $event) {
 			} */
 			
 			//if(trim($result_text) == ''){ //search in EN
-				$ch1 = curl_init();
-				curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-				curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch1, CURLOPT_URL, 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[2]);
-				$result1 = curl_exec($ch1);
-				curl_close($ch1);
+				$ch2 = curl_init();
+				curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch2, CURLOPT_URL, 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[2]);
+				$result2 = curl_exec($ch2);
+				curl_close($ch2);
 			
-				$obj = json_decode($result1, true);
+				$obj = json_decode($result2, true);
 			
 				foreach($obj['query']['pages'] as $key => $val){ 
 					$result_text = $val['extract']; 
